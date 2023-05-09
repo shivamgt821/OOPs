46bc1abbae1b85cdc65154cea7a36d0d21037f90
SHIVAM KUMAR GUPTA <120335161+shivamgt821@users.noreply.github.com>
2023-05-09 22:52:00 +0530

    Create OuterClass.java

---
+++ OuterClass.java
@@ -0,0 +1,11 @@
+public class OuterClass {
+	int a;
+	void m1() {
+		System.out.println("m1 for outer class");
+	}
+	class InnerClass{
+		void m2() {
+			System.out.println("m2 for inner class");
+		}
+	}
+}
